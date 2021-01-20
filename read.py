1d8b0145cb78c91e93170c90ac5ed9b7693cb566
Anna Chang <p227738930@hotmail.com>
2021-01-20 20:22:25 +0800

    calc avg len reviews

--- read.py
+++ read.py
@@ -6,8 +6,12 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+print('檔案讀取完畢, 總共有', len(data), '筆評論')
 
-print(data[0])
-print('-------------------------')
-print(data[199])
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	print(sum_len)
+
+print('留言平均長度為', sum_len/len(data))
+
