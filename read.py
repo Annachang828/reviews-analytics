b28439bac358b8617ac8e2001f8b41df150cd865
Anna Chang <p227738930@hotmail.com>
2021-01-20 20:42:42 +0800

    add filtering function

--- read.py
+++ read.py
@@ -11,7 +11,12 @@ print('檔案讀取完畢, 總共有', len(data), '筆評論')
 sum_len = 0
 for d in data:
 	sum_len += len(d)
-	print(sum_len)
-
 print('留言平均長度為', sum_len/len(data))
 
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '超過100個字母')
+print(new[0])
+print(new[1])
